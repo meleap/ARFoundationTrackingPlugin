e218fded825b7ea66c48da767bdbac154cd2b01d
丹羽雄大 <yudai@niwayuudainoMacBook-puro.local>
2020-05-13 19:41:01 +0900

    検出されたマーカの相対位置に基づいて原点を調整する処理を追加

--- Assets/Scenes/ImageTracking/TrackedImageInfoManager.cs
+++ Assets/Scenes/ImageTracking/TrackedImageInfoManager.cs
@@ -12,6 +12,22 @@ using UnityEngine.XR.ARFoundation;
 [RequireComponent(typeof(ARTrackedImageManager))]
 public class TrackedImageInfoManager : MonoBehaviour
 {
+
+    [Tooltip("AR座標系の回転のうちY軸方向のみを修正するか,falseの場合は全ての軸で」調整を行う")]
+    public bool isOnlyAdjustYAxis = true; // AR座標系の回転のうちY軸方向のみを修正するか
+
+    // 前回の座標系調整時間
+    private float _previousAdjustTime = 0; 
+
+    [Tooltip("AR座標系の基準")]
+    Transform _arOriginTransform = null;
+
+    // マーカの名前からUnity座標系におけるマーカーのTransformを引くための辞書
+    private Dictionary<string, Transform> _mDicMarkerNameToTransform;
+
+    // 座標系を時間をかけて調整するためのクラス
+    private TransformMover _arOriginMover;
+
     [SerializeField]
     [Tooltip("The camera to set on the world space UI canvas for each instantiated image info.")]
     Camera m_WorldSpaceCanvasCamera;
@@ -55,6 +71,67 @@ public class TrackedImageInfoManager : MonoBehaviour
     void OnDisable()
     {
         m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    // マーカTransform用のY軸回転角度計算
+    // マーカは画像面から手前方向がY軸正のため、マーカを壁に貼るとマーカ上方はZ軸正となる
+    float GetMarkerYAngle(Quaternion rotation)
+    {
+        var forward = rotation * Vector3.down;
+        float yAngle = Mathf.Atan2(forward.x, forward.z);
+        return yAngle;
+    }
+
+    // 検出されたマーカの相対位置に基づいて原点を調整する
+    private void UpdateOrigin(ARTrackedImage trackedImage)
+    {
+        // トラッキングしていない画像は処理しない
+        if (trackedImage.trackingState == TrackingState.None)
+        {
+            return;
+        }
+
+        // マーカに対応するTransformを取得
+        if (!_mDicMarkerNameToTransform.ContainsKey(trackedImage.referenceImage.name))
+        {
+            Debug.LogError("not found name: " + trackedImage.referenceImage.name);
+            return;
+        }
+
+        // 検出した画像の位置姿勢がUnity座標系の対応マーカーの位置姿勢と同じになるようにAROriginの位置姿勢を計算
+        // スケールは調整しない
+        var markerTransform = _mDicMarkerNameToTransform[trackedImage.referenceImage.name];
+        var trackedImageTransform = trackedImage.transform;
+        Quaternion rotation;
+        Vector3 position;
+        if (isOnlyAdjustYAxis)
+        {
+            // 垂直方向が変化しないようにY回転のみ調整する
+            Debug.Log(GetMarkerYAngle(markerTransform.rotation) * Mathf.Rad2Deg + ", "  + GetMarkerYAngle(trackedImageTransform.localRotation) * Mathf.Rad2Deg);
+
+            float correctYAngle = GetMarkerYAngle(markerTransform.rotation) - GetMarkerYAngle(trackedImageTransform.localRotation);
+
+            rotation = Quaternion.AngleAxis(correctYAngle * Mathf.Rad2Deg, Vector3.up);
+            position = markerTransform.position - rotation * trackedImageTransform.localPosition;
+        }
+        else
+        {
+            // 姿勢も完全にマーカ基準で調節する
+            rotation = markerTransform.rotation * Quaternion.Inverse(trackedImageTransform.localRotation);
+            position = markerTransform.position - _arOriginTransform.rotation * trackedImageTransform.localPosition;
+        }
+
+        // AROriginの調整開始
+        _arOriginMover.StartMove(position, rotation);
+
+        _previousAdjustTime = Time.time;
+
+        //Debug.Log(
+        //    "global position: " + trackedImageTransform.position + Environment.NewLine
+        //    + "global rotation: " + trackedImageTransform.rotation + Environment.NewLine
+        //    + "local position: " + trackedImageTransform.localPosition + Environment.NewLine
+        //    + "local rotation: " + trackedImageTransform.localRotation + Environment.NewLine
+        //    );
     }
 
     void UpdateInfo(ARTrackedImage trackedImage)
