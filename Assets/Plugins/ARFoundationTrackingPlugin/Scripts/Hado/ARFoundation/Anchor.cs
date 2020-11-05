98a2d11623eeeb055c708a2d554e59c6bc2adc14
trapple <trapplejp@gmail.com>
2020-11-05 11:32:32 +0900

    ArrowはDebugBuildだったら表示しない

--- Assets/Plugins/ARFoundationTrackingPlugin/Scripts/Hado/ARFoundation/Anchor.cs
+++ Assets/Plugins/ARFoundationTrackingPlugin/Scripts/Hado/ARFoundation/Anchor.cs
@@ -1,11 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Hado.ARFoundation
 {
     public class Anchor : MonoBehaviour
     {
+        [SerializeField] private GameObject[] _debugObjects;
+        
         public string Name { get; set; }
+        
+        private void Awake()
+        {
+            foreach (var obj in (_debugObjects))
+            {
+                obj.SetActive(Debug.isDebugBuild);
+            }
+        }
     }
 }
