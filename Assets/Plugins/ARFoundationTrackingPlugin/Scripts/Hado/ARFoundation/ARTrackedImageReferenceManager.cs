8e94957f73c9dc32fdade6a7ba7131dcfc9c40c2
trapple <trapplejp@gmail.com>
2020-12-09 16:47:57 +0900

    マーカー複数扱う

---
+++ Assets/Plugins/ARFoundationTrackingPlugin/Scripts/Hado/ARFoundation/ARTrackedImageReferenceManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
+
+namespace Hado.ARFoundation
+{
+    public class ARTrackedImageReferenceManager : MonoBehaviour
+    {
+        [SerializeField] public XRReferenceImageLibrary[] MarkerSets;
+
+        private int _defaultMarkerSet = 0;
+        
+        void Awake()
+        {
+            if(MarkerSets.Length == 0)
+                Debug.LogError("MarkerSets is not defined");
+        }
+
+        public XRReferenceImageLibrary GetMarkerSet(int num)
+        {
+            if(num > MarkerSets.Length)
+                throw new Exception("invalid marker set number");
+            return MarkerSets[num];
+        }
+    }
+}
