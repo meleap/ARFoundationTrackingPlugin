532bb6256840e3ea2c8e88c26e3bc2aed551ea5c
trapple <trapplejp@gmail.com>
2021-01-25 19:14:09 +0900

    さんぷる更新

--- Assets/Examples/Scripts/UIARCameraPositionView.cs
+++ Assets/Examples/Scripts/UIARCameraPositionView.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Hado.ARFoundation;
+using UniRx;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.ARFoundation;
 
@@ -22,6 +23,11 @@ public class UIARCameraPositionView : MonoBehaviour
         _arCamera = ARSessionManager.Instance.arCamera.transform;
 
         ARSession.stateChanged += UpdateARStateChangedText;
+
+        ARSessionManager.Instance.arTrackedImageEventManager.OnTrackedImagesChangedObservable
+            .Select(x => x.gameObject.transform.position)
+            .Buffer(3)
+            .Subscribe(UpdatePositionText);
     }
 
     private void Update()
@@ -44,6 +50,37 @@ public class UIARCameraPositionView : MonoBehaviour
         _uiText.text += $"rot: ({ea.x:f4}, {ea.y:f4}, {ea.z:f4})\n";
 
         _uiText.text += $"current: {_pm.LastDetectedAnchorName}";
+    }
+
+    private void UpdatePositionText(GameObject go)
+    {
+        var tr = go.transform;
+        _uiText.text = $"pos: ({tr.position.x:f6}, {tr.position.y:f6}, {tr.position.z:f6}) - ";
+
+        var ea = tr.rotation.eulerAngles;
+        _uiText.text += $"rot: ({ea.x:f4}, {ea.y:f4}, {ea.z:f4})\n";
+
+        _uiText.text += $"current: {_pm.LastDetectedAnchorName}"; 
+    }
+    
+    private void UpdatePositionText(IList<GameObject> go)
+    {
+        var tr = go[0].transform;
+        var tr1 = go[1].transform;
+        _uiText.text = $"pos: ({tr.position.x:f6}, {tr.position.y:f6}, {tr.position.z:f6})\n";
+
+        _uiText.text += $"pos: ({tr1.position.x:f6}, {tr1.position.y:f6}, {tr1.position.z:f6})\n";
+
+        _uiText.text += $"current: {_pm.LastDetectedAnchorName}"; 
+    }
+    
+    private void UpdatePositionText(IList<Vector3> pos)
+    {
+        _uiText.text = $"pos: ({pos[0].x:f6}, {pos[0].y:f6}, {pos[0].z:f6})\n";
+
+        _uiText.text += $"pos: ({pos[1].x:f6}, {pos[1].y:f6}, {pos[1].z:f6})\n";
+
+        _uiText.text += $"current: {_pm.LastDetectedAnchorName}"; 
     }
 
     private void OnDestroy()
