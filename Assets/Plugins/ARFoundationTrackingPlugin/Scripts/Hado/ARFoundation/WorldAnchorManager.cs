be7b0b58485eadcd96eaacf45a4033f4a1994aec
trapple <trapplejp@gmail.com>
2021-01-26 13:47:04 +0900

    AB,BCの移動距離を見る

--- Assets/Plugins/ARFoundationTrackingPlugin/Scripts/Hado/ARFoundation/WorldAnchorManager.cs
+++ Assets/Plugins/ARFoundationTrackingPlugin/Scripts/Hado/ARFoundation/WorldAnchorManager.cs
@@ -11,36 +11,44 @@ using UnityEngine.XR.ARSubsystems;
 
 namespace Hado.ARFoundation
 {
+    public enum MovingStatus
+    {
+        None,
+        Detecting,
+        Moving
+    }
+    
     public class WorldAnchorManager : MonoBehaviour
     {
-        /// <summary>
-        /// プルつき防止のために、マーカー位置のズレがMovingThreshold以下の場合はWorldAnchorを動かさない
-        /// </summary>
-        [NonSerialized] public float MovingStartThreshold = 0.05f;
-
         /// <summary>
         /// フレーム間の移動距離がこの値より大きい場合はノイズとして捨てる
         /// </summary>
         [NonSerialized] public float MovingNoiseThreshold = 0.05f;
-        
+
+        /// <summary>
+        /// MovingNoiseThresholdのチェックを何回ぶん行うか
+        /// </summary>
+        [NonSerialized] public int NoiseCheckSampleCount = 2;
+
         /// <summary>
         /// 予定位置からの距離がこの値になったら移動を停止する
         /// </summary>
-        [NonSerialized]
-        public float MovingEndThreshold = 0.01f;
+        [NonSerialized] public float MovingEndThreshold = 0.01f;
 
         /// <summary>
         /// SmoothDumpのSmoothTime, 1fなら1秒かける、というわけでもない（ちょっと長い）, 0.2f前後で調整推奨
         /// </summary>
-        [NonSerialized]
-        public float SmoothTime = 0.2f;
-        
+        [NonSerialized] public float SmoothTime = 0.2f;
+
         private Transform _transform;
         private Vector3 _moveEndPosition;
         private Quaternion _moveEndRotation;
-        
-        private bool _isMoving;
+
         private Vector3 _velocity = Vector3.zero;
+
+        private List<float> _noiseCheckSamples = new List<float>();
+
+        public ReactiveProperty<MovingStatus> IsMoving = new ReactiveProperty<MovingStatus>(MovingStatus.None);
 
         private void Awake()
         {
@@ -51,58 +59,73 @@ namespace Hado.ARFoundation
         private void Start()
         {
             ARSessionManager.Instance.arTrackedImageEventManager.OnTrackedImagesChangedObservable
-                .Where(_ => !_isMoving) // 補正中は流さない
+                .Where(_ => IsMoving.Value == MovingStatus.None) // 補正中は流さない
                 .Do(t => PositionManager.Instance.LastDetectedAnchorName = t.referenceImage.name)
                 .Do(t => Debug.Log($"{t.referenceImage.name} detected"))
-                .Select(t =>  ARSessionManager.Instance.arTrackedImageEventManager.GetReferenceAnchor(t.referenceImage.name).transform.position)
+                .Select(t =>
+                    ARSessionManager.Instance.arTrackedImageEventManager.GetReferenceAnchor(t.referenceImage.name)
+                        .transform.position)
                 .Where(_ => ARSession.state >= ARSessionState.SessionTracking)
-                .Buffer(3)
+                .Buffer(NoiseCheckSampleCount + 1)
                 .Subscribe(positions =>
                 {
-                    // フレーム間の移動距離が大きすぎる場合はノイズとして捨てる
-                    if (IsNoiseData(positions)) return;
                     
+                    // フレーム間の移動距離が大きすぎる場合はノイズとして捨てる
+                    IsMoving.Value = MovingStatus.Detecting;
+                    _noiseCheckSamples.Clear();
+                    if (IsNoiseData(positions))
+                    {
+                        IsMoving.Value = MovingStatus.None;
+                        return;
+                    };
+
                     _moveEndPosition = positions[2];
                     _moveEndRotation =
                         ARSessionManager.Instance.arTrackedImageEventManager.GetReferenceAnchor(PositionManager.Instance
                             .LastDetectedAnchorName).transform.rotation;
 
-                    _isMoving = true;
+                    IsMoving.Value = MovingStatus.Moving;
                 }).AddTo(this);
-            
+
             // Transformのズレを補正
             Observable.EveryUpdate()
-                .Where(_ => _isMoving)
+                .Where(_ => IsMoving.Value == MovingStatus.Moving)
                 .Subscribe(_ =>
                 {
-                    _transform.position = Vector3.SmoothDamp(_transform.position, _moveEndPosition, ref _velocity, SmoothTime);
+                    _transform.position =
+                        Vector3.SmoothDamp(_transform.position, _moveEndPosition, ref _velocity, SmoothTime);
 
                     // まだMovingEndThresholdより距離があるので継続
                     if (Vector3.Distance(_transform.position, _moveEndPosition) > MovingEndThreshold) return;
-                    
+
                     Debug.Log($"Moving finished");
-                    _isMoving = false;
+                    IsMoving.Value = MovingStatus.None;
                     _velocity = Vector3.zero;
-                    
+
                     // rotationはさほどずれることはない前提でなめさないで一気に入れる
                     _transform.SetPositionAndRotation(_transform.position, _moveEndRotation);
-                    
                 }).AddTo(this);
         }
 
         private bool IsNoiseData(IList<Vector3> positions)
         {
-            var dist1 = Vector3.Distance(positions[0], positions[1]);
-            var dist2 = Vector3.Distance(positions[1], positions[2]);
-            Debug.Log($"MovingNoiseThreshold: {Math.Abs(dist1 - dist2):F6}");
-            return Math.Abs(dist1 - dist2) > MovingNoiseThreshold;
+            for (var i = 0; i < NoiseCheckSampleCount; i++)
+            {
+                _noiseCheckSamples.Add(Vector3.Distance(positions[i], positions[i + 1]));
+                Debug.Log($"Noise check[{i}]: {Vector3.Distance(positions[i], positions[i + 1]):F6}");
+            }
+            
+            Debug.Log($"Check: {_noiseCheckSamples.Any(x => x > MovingEndThreshold)}");
+
+            return _noiseCheckSamples.Any(x => x > MovingEndThreshold);
         }
-        
-        public IDisposable RegisterIntervalTracking(CancellationTokenSource cancellationTokenSource, int imageTrackingIntervalMils = 3000)
+
+        public IDisposable RegisterIntervalTracking(CancellationTokenSource cancellationTokenSource,
+            int imageTrackingIntervalMils = 3000)
         {
             Debug.Log("RegisterIntervalTracking");
             return ARSessionManager.Instance.arTrackedImageEventManager.OnTrackedImagesChangedObservable
-                .Where(_ => _isMoving) // 補正が始まったら発火
+                .Where(_ => IsMoving.Value == MovingStatus.Moving) // 補正が始まったら発火
                 .Subscribe(async _ =>
                     {
                         ARSessionManager.Instance.EnabledImageTracking = false;
@@ -117,14 +140,13 @@ namespace Hado.ARFoundation
                     }
                 );
         }
-        
+
         private async UniTask WaitForMoveEnd()
         {
-            while (_isMoving)
+            while (IsMoving.Value == MovingStatus.Moving)
             {
                 await UniTask.NextFrame();
             }
         }
-
     }
 }
