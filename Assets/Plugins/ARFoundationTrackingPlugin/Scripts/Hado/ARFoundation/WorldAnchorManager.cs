b475cc347295b565b806e1c0836c44bb15283091
doyasu24 <doyasu24@gmail.com>
2024-09-19 20:07:26 +0900

    Editorデバッグ用の機能

--- Assets/Plugins/ARFoundationTrackingPlugin/Scripts/Hado/ARFoundation/WorldAnchorManager.cs
+++ Assets/Plugins/ARFoundationTrackingPlugin/Scripts/Hado/ARFoundation/WorldAnchorManager.cs
@@ -166,6 +166,14 @@ namespace Hado.ARFoundation
             }
         }
 
+        private void Update()
+        {
+            // デバッグ用に、Editorだったらインスペクタでposition, rotationが変更されたことを検知する
+            if (!Application.isEditor) return;
+            var t = (_transform.position, _transform.rotation);
+            if (_positionAndRotation.Value != t) _positionAndRotation.Value = t;
+        }
+
         private void OnDestroy()
         {
             _positionAndRotation.Dispose();
